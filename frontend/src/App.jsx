f1f250fc462e9c612a363b763b0b9fb7c691ed0d
Piyush Channawar <channawarpiyush@gmail.com>
2024-12-22 11:19:07 +0530

    add routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Projects from './pages/Projects'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <>
-   <h1 className='text-3xl font-serif underline'>Hello world</h1>
-   </>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/project" element={<Projects />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
