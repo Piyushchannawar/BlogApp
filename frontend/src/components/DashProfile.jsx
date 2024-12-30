583f1ff84b11ff11c6203f8629b8bf0c9870a381
Piyush Channawar <channawarpiyush@gmail.com>
2024-12-31 00:49:47 +0530

    add profile image upload functionality; implement image preview and Cloudinary integration

--- frontend/src/components/DashProfile.jsx
+++ frontend/src/components/DashProfile.jsx
@@ -1,16 +1,49 @@
 import { Button, TextInput } from 'flowbite-react'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 
 const DashProfile = () => {
     const {currentUser} = useSelector(state => state.user)
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const filePickerRef = useRef();
+    const handleImageChange = async (e) => {
+        const file = e.target.files[0];
+        setImageFile(e.target.files[0]);
+        if(file){
+            setImageFile(file);
+            setImageFileUrl(URL.createObjectURL(file));
+        }
+        const data = new FormData();
+        data.append('file', file);
+        data.append('upload_preset', 'blogapp');
+        data.append('cloud_name', 'doeicc3sy');
+
+        const response = await fetch('https://api.cloudinary.com/v1_1/doeicc3sy/image/upload', {
+            method: 'POST',
+            body: data
+        });
+        const jsonResponse = await response.json();
+        setImageFileUrl(jsonResponse.URL);
+        console.log(jsonResponse.URL);
+    };
+   useEffect(() => {
+    if(imageFile){
+        uploadImage();
+    }
+   }, [imageFile]);
+    const uploadImage = async () => {
+    };
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
         <form className='flex flex-col gap-4'>
-            <div className='w-32 h-32 self-center curser-pointer shadow-md overflow-hidden rounded-full'>
-    <img src={currentUser.profilePicture} alt="user" className='rounded-full w-full h-full object-cover border-8 border-[lightgray]' />
+            <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} 
+            hidden
+            />
+            <div className='w-32 h-32 self-center curser-pointer shadow-md overflow-hidden rounded-full' onClick={()=> filePickerRef.current.click()}>
+    <img src={imageFileUrl ||  currentUser.profilePicture} alt="user" className='rounded-full w-full h-full object-cover border-8 border-[lightgray]' />
             </div>
             <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} />
             <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} />
