573414f04559588478c862d0e9626fd005a17722
Piyush Channawar <channawarpiyush@gmail.com>
2024-12-30 18:28:30 +0530

    add dashboard functionality; implement private route, sidebar, and profile components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from './pages/Signup'
 import Projects from './pages/Projects'
 import Header from './components/Header'
 import FooterCom from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 
 function App() {
@@ -17,11 +18,13 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/home" element={<Home />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/signup" element={<Signup />} />
+      <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
     </Routes>
     <FooterCom />
   </BrowserRouter>
