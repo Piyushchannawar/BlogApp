a23da6f82e6471346c8ab48a82152c32140e43ca
Piyush Channawar <channawarpiyush@gmail.com>
2024-12-31 15:03:30 +0530

    add user deletion functionality; implement delete user API endpoint, Redux actions, and confirmation modal in profile component

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, test2, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, test, test2, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.get("/", test);
 router.get("/test", test2);
 router.put("/update/:userId", verifyToken ,updateUser)
+router.delete("/delete/:userId", verifyToken, deleteUser);
 
 export default router;
