d119aa820346d0e64f3dfa42f4a4ad3fc2773943
Piyush Channawar <channawarpiyush@gmail.com>
2025-01-03 23:32:09 +0530

    add post submission functionality; implement form handling, error display, and navigation upon successful post creation in CreatePost component

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -2,11 +2,15 @@ import {Button, FileInput, Select, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-
+import {useNavigate} from 'react-router-dom';
 
 const  CreatePost = () => {
   const [file, setFile] = useState(null);
   const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+  console.log(formData);
+  const navigate = useNavigate();
+  
   const handleImageUpload = async () =>{
     if(file){
       const data = new FormData();
@@ -23,10 +27,38 @@ const  CreatePost = () => {
       console.log(jsonResponse.url);
     }
   };
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    const res = await fetch('/api/post/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+
+    if (!res.ok) {
+      setPublishError(data.message);
+      return;
+    }
+    if(data.slug){
+      navigate(`/post/${data.slug}`);
+    }
+  } catch (error) {
+    setPublishError(error.message);
+  }
+};
+
+
+
+
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit} >
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
             type='text'
@@ -34,12 +66,15 @@ const  CreatePost = () => {
             required
             id='title'
             className='flex-1'
+            onChange={(e) => setFormData({ ...formData, title: e.target.value })}
           />
-          <Select>
+          <Select
+          onChange={(e) => setFormData({ ...formData, category: e.target.value })}
+          >
             <option value='uncategorized'>Select a category</option>
-            <option value='javascript'>Web Development</option>
-            <option value='reactjs'>DSA</option>
-            <option value='nextjs'>Machine Learning</option>
+            <option value='Webdevelopment'>Web Development</option>
+            <option value='Dsa'>DSA</option>
+            <option value='MachineLearning'>Machine Learning</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
@@ -64,10 +99,12 @@ const  CreatePost = () => {
           placeholder='Write something...'
           className='h-72 mb-12'
           required
+          onChange={(value) => setFormData({ ...formData, content: value })}
         />
         <Button type='submit' gradientDuoTone='purpleToPink'>
           Publish
         </Button>
+        {publishError && <p className='text-red-500'>{publishError}</p>}
       </form>
     </div>
   );
